ea003780d4e5ea7a3f9044515037095c7ed54028
Jul-Wie <jwdeking@gmail.com>
2020-10-05 14:29:19 +0200

    Update 1.1

--- stuff.cpp
+++ stuff.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 #include "stuff.h"
 bool continue_ = false;
+bool failed_ = false;
+void checkinput(){
+
+    if(std::cin.fail())
+        {
+            std::cin.clear();
+
+            std::cout << "Invalid input " << std::endl;
+            continue_ = false;
+            program();
+            failed_ = true;
+        }
+   else failed_ = false;
+}
 void lowerCase(std::string& strToConvert)
 {
 
@@ -19,23 +33,20 @@ int program(){
     std::cout << std::endl;
     }
     if(start != "Z8+YE[EZ^Dy8=wj>DUvkwpp?@dz8G!U?zpMUd9^}<s6%M6S&}$:v:>"){
-    std::cout << "You have the options: square, double, half and sixnine. Enter 'exit' to exit." << std::endl;
+    std::cout << "You have the options: power, times and divide. Enter 'exit' to exit." << std::endl;
     std::string input;
     std::cin >> input;
     lowerCase(input);
-    if(input == "square"){
-        square();
+    if(input == "power"){
+        power();
     }
-    else if(input == "double"){
-        doubled();
+    else if(input == "times"){
+        times();
     }
-    else if(input == "half"){
-        half();
+    else if(input == "divide"){
+        divide();
     }
 
-    else if(input == "6ix9ine"){
-        sixnine();
-    }
     else if (input == "exit"){
     return 0;
     }
@@ -48,76 +59,67 @@ int program(){
     }
 }
 
- int square(){
-    int in;
-    std::cout << "Please specify what to square, or enter 'exit' to exit." << std::endl;
-    std::cin >> in;
-    if(std::cin.fail())
-        {
-            std::cin.clear();
-
-            std::cout << "Invalid input " << std::endl;
-            continue_ = false;
-            program();
-        }else{
-    in = in * in;
-    std::cout << in << std::endl;
+ int power(){
+    long long in1;
+    long long in2;
+    long long result;
+    std::cout << "Please specify what to power, or enter 'exit' to exit." << std::endl;
+    std::cin >> in1;
+    result = in1;
+    checkinput();
+    if(!failed_){
+            std::cout << "Okay, now what power? (still 'exit') to exit." << std::endl;
+            std::cin >> in2;
+            checkinput();
+    if(!failed_){
+            for(int i = in2; i > 1; i--){
+            result = result * in1;
+            std::cout << result << std::endl;
+            }
+    }
     continue_ = true;
     program();
     }
-}
- int doubled(){
-    int in;
-    std::cout << "Please specify what to double, or enter 'exit' to exit." << std::endl;
-    std::cin >> in;
-    if(std::cin.fail())
-        {
-            std::cin.clear();
+    }
 
-            std::cout << "Invalid input " << std::endl;
-            continue_ = false;
-            program();
-        }else{
-    in = in * 2;
-    std::cout << in << std::endl;
+ int times(){
+    int in1;
+    int in2;
+    int result;
+    std::cout << "Please specify what to times, or enter 'exit' to exit." << std::endl;
+    std::cin >> in1;
+   checkinput();
+    if(!failed_){
+            std::cout << "Okay, how many times that? (or 'exit' to exit.)" << std::endl;
+            std::cin >> in2;
+           checkinput();
+    if(!failed_){
+    result = in1 * in2;
+    std::cout << result << std::endl;
     continue_ = true;
     program();
-    return 0;}
+    return 0;
+        }
+    }
 }
- int half(){
-    int in;
-    std::cout << "Please specify what to half, or enter 'exit' to exit." << std::endl;
-    std::cin >> in;
-    if(std::cin.fail())
-        {
-            std::cin.clear();
-
-            std::cout << "Invalid input " << std::endl;
-            continue_ = false;
-            program();
-        }else{
-    in = in / 2;
-    std::cout << in << std::endl;
+ int divide(){
+    int in1;
+    int in2;
+    int result;
+    std::cout << "Please specify what to divide, or enter 'exit' to exit." << std::endl;
+    std::cin >> in1;
+    checkinput();
+    if(!failed_){
+            std::cout << "okay, by what to divide that? (or 'exit' to exit.)" << std::endl;
+            std::cin >> in2;
+    result = in1 / in2;
+        checkinput();
+    if(!failed_){
+    std::cout << result << std::endl;
     continue_ = true;
     program();
-    return 0;}
-}
- int sixnine(){
-    int in;
-    std::cout << "Please specify what to 6ix9ine, or enter 'exit' to exit." << std::endl;
-    std::cin >> in;
-    if(std::cin.fail())
-        {
-            std::cin.clear();
-
-            std::cout << "Invalid input " << std::endl;
-            continue_ = false;
-            program();
-        }else{
-    in = in * 69;
-    std::cout << in << std::endl;
-    continue_ = true;
-    program();
-    return 0;}
+    return 0;
+        }
+    }
 }
 
